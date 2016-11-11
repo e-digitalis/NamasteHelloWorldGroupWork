a4c2119cca9bc2cf0af9fad310fd72e7a4032a04
User <User@FDC7M82>
2016-11-11 16:12:36 +0100

    Namaste test

---
+++ src/namasteTest/MainNamasteTest.java
@@ -0,0 +1,12 @@
+package namasteTest;
+
+public class MainNamasteTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Namaste! Hello world!");
+		System.out.println("Let's do this!");
+		
+	}
+
+}
